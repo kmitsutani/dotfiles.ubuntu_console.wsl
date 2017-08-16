58350111367866405f4283766568ece040232eb3
Kazuya Mitsutani <kzymtn@gmail.com>
2017-08-16 19:06:27 +0900

    extern src-dist pairs

--- link.sh
+++ link.sh
@@ -2,44 +2,92 @@
 wd=$(cd $(dirname $0); pwd)
 cd $wd
 
-# bash
-ln -s $wd/bash_profile $HOME/.bash_profile
-ln -s $wd/bash_profile_path $HOME/.bash_profile_path
+_lndot(){
+  line=$1  
+  src_dist=($(sed -e "s/:/ /" <<< $line))
+  src=${src_dist[0]}
+  dist=${src_dist[1]}
+  if [ "$dist" == "" ]
+  then
+    dist=".$src"
+  fi
+  fullsrc=$wd/$src
+  fulldist=$HOME/$dist
+  if [ -f $fulldist ]
+  then
+    mv $fulldist $fulldist.old
+  fi
+  echo ln -s $fullsrc $fulldist
+  ln -s $fullsrc $fulldist
+}
 
-ln -s $wd/profile_ssh_agent $HOME/.profile_ssh_agent
-if [ -f $HOME/.bashrc ]; then
-  mv $HOME/.bashrc $HOME/.bashrc_old
-  ln -s $wd/bashrc $HOME/.bashrc
-else
-  ln -s $wd/bashrc $HOME/.bashrc
+src_notfound=0
+cat srcdist.txt|\
+while read line
+do
+  [ "$line" == "" ] && continue
+  [[ "$line" =~ ^# ]] && continue
+  [[ "$line" =~ ^@ ]] && continue
+  src=$(echo $line | cut -f 1 -d :)
+  if [ ! -f $wd/$src ] && [ ! -d $wd/$src ]; then
+    echo "$wd/$src is not found"
+    src_notfound=1
+  fi
+done
+if [ ${src_notfound} -gt 0 ]; then
+  "some files not found."
+  exit 0
 fi
 
-# vim
-ln -s $wd/vimrc $HOME/.vimrc
-ln -s $wd/vimrc_dein $HOME/.vimrc_dein
-[ ! -d $HOME/.vim/rc ] && mkdir -p $HOME/.vim/rc
-ln -s $wd/vim/rc/dein.toml $HOME/.vim/rc/dein.toml
-ln -s $wd/vim/rc/dein_lazy.toml $HOME/.vim/rc/dein_lazy.toml
+cat srcdist.txt|\
+while read line
+do
+  [ "$line" == "" ] && continue
+  [[ "$line" =~ ^# ]] && continue
+  # @-line
+  if [[ "$line" =~ ^@ ]];
+  then
+    key_val=($(sed -e "s/=/ /" <<< $line))
+    key=${key_val[0]}
+    vals=($(sed -e "s/:/ /" <<< ${key_val[1]}))
+    case $key in
+      '@reqcmd' )
+        for cmd in ${vals[@]}
+        do
+          if [ $(which $cmd | wc -l) -eq 0 ]; then
+            echo "$cmd not found"
+            exit 0
+          fi
+        done
+        ;;
+      '@reqdir' )
+        for dir in ${vals[@]}
+        do
+          [ ! -d $HOME/$dir ] && mkdir -p $HOME/$dir
+        done
+        ;;
+    esac
+  else
+    _lndot $line
+  fi
+done
 
-# ssh_config
-[ ! -d $HOME/.ssh ] && mkdir -p $HOME/.ssh
-ln -s $wd/ssh_config $HOME/.ssh/config
+# change permission of ssh_config
+cp $wd/ssh_config $HOME/.ssh/config
 chmod 600 $HOME/.ssh/config
 
-# tmux
-ln -s $wd/tmux.conf $HOME/.tmux.conf
+# tmux-mouse
 [ ! -d $HOME/.tmux ] && mkdir -p $HOME/.tmux
 _tmuxver=$(tmux -V | sed -e 's/[^0-9.]//g')
 if [ $(bc <<< "$_tmuxver > 2.0") -ne 0 ]; then
-  ln -s $wd/tmux/mouse-after-2.1.conf $HOME/.tmux/mouse.conf
+  ln -sf $wd/tmux/mouse-after-2.1.conf $HOME/.tmux/mouse.conf
 else
-  ln -s $wd/tmux/mouse-before-2.0.conf $HOME/.tmux/mouse.conf
+  ln -sf $wd/tmux/mouse-before-2.0.conf $HOME/.tmux/mouse.conf
 fi
 unset _tmuxver
 
 # xkb (for X11)
 if [ -d /etc/X11 ]; then
-  ln -s $wd/xkb $HOME/.xkb
   echo "use $HOME/.xkb/activate as launcher of setting"
 fi
 
